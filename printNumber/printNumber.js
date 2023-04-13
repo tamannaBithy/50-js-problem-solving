c180916d2b5ca75db458c44c3bc8c568cd3c4213
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-13 17:00:28 +0600

    print number from 1 to 10

---
+++ printNumber/printNumber.js
@@ -0,0 +1,7 @@
+// #1. Coding challenge #1: Print numbers from 1 to 10
+function printNumber() {
+    for (var i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+printNumber();
---
+++ printNumber/printNumber.ts
@@ -0,0 +1,9 @@
+// #1. Coding challenge #1: Print numbers from 1 to 10
+
+function printNumber() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+
+printNumber();
