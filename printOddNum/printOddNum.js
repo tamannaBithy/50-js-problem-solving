7cb10a3a58d94c124d57bcb7282c11282a1f62bb
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-17 18:22:10 +0600

    print odd number

--- printOddNum/printOddNum.js
+++ printOddNum/printOddNum.js
@@ -1,3 +1,13 @@
 "use strict";
 // #2. Coding challenge #2: Print the odd numbers less than 100
-function printOddNumber() { }
+function printOddNumber() {
+    //   for (let i = 0; i < 100; i++) {
+    //     if (i % 2 !== 0) {
+    //       console.log(i);
+    //     }
+    //   }
+    for (let i = 1; i < 100; i += 2) {
+        console.log(i);
+    }
+}
+printOddNumber();
