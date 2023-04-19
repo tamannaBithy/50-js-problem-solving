b80dea02bbdb076ebb6793527598e1b54072ba77
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-19 18:24:40 +0600

    sum of odd numbers

---
+++ sumOddNumb/sumOddNumb.js
@@ -0,0 +1,10 @@
+"use strict";
+// #7: Calculate the sum of odd numbers greater than 10 and less than 30
+function sumOddNumber() {
+    let sum = 0;
+    for (let i = 11; i < 30; i += 2) {
+        sum += i;
+    }
+    console.log(sum);
+}
+sumOddNumber();
