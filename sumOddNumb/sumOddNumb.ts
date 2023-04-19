c8d893bf630a05f091aa478a5627c24698eb2fce
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-19 18:28:00 +0600

    number type added

--- sumOddNumb/sumOddNumb.ts
+++ sumOddNumb/sumOddNumb.ts
@@ -1,7 +1,7 @@
 // #7: Calculate the sum of odd numbers greater than 10 and less than 30
 
 function sumOddNumber() {
-    let sum = 0;
+    let sum : number = 0;
     for (let i = 11; i < 30; i += 2){
         sum += i
     }
