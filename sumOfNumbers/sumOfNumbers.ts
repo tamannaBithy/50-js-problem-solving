a878bb2496bf70b678744d4797dcb74d75eb75de
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-19 18:29:24 +0600

    array type added

--- sumOfNumbers/sumOfNumbers.ts
+++ sumOfNumbers/sumOfNumbers.ts
@@ -1,11 +1,11 @@
 // #5: Calculate the sum of numbers from 1 to 10
 
 function sumOfNumbers() {
-  let newArr = [];
+  let newArr : number[] = [];
   for (let i = 1; i <= 10; i++) {
     newArr.push(i);
   }
-  const sum = newArr.reduce((a, b) => a + b, 0);
+  const sum : number = newArr.reduce((a, b) => a + b, 0);
   console.log(sum);
 }
 
@@ -14,7 +14,7 @@ sumOfNumbers();
 ///////////////// ---------------- 2nd way ---------------------- /////////////////////
 
 function sumTheNumbers() {
-  let sum = 0;
+  let sum : number = 0;
   for (let i = 0; i <= 10; i++) {
     sum += i;
   }
