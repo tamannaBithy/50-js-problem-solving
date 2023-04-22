2839696cb3d50b2b79652b223cbb3ee76391161a
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-22 16:02:59 +0600

    completed 11 problem

--- problems.js
+++ problems.js
@@ -2,13 +2,13 @@
 // ✌ #2. Coding challenge #2: Print the odd numbers less than 100
 // ✌ #3: Print the multiplication table with 7
 // ✌ #4: Print all the multiplication tables with numbers from 1 to 10
-// #5: Calculate the sum of numbers from 1 to 10
-// Skip - #6: Calculate 10!
-// #7: Calculate the sum of odd numbers greater than 10 and less than 30
-// #8: Create a function that will convert from Celsius to Fahrenheit
-// #9: Create a function that will convert from Fahrenheit to Celsius
-// #10: Calculate the sum of numbers in an array of numbers
-// #11: Calculate the average of the numbers in an array of numbers
+// ✌ #5: Calculate the sum of numbers from 1 to 10
+// ✌ Skip - #6: Calculate 10!
+// ✌ #7: Calculate the sum of odd numbers greater than 10 and less than 30
+// ✌ #8: Create a function that will convert from Celsius to Fahrenheit
+// ✌ #9: Create a function that will convert from Fahrenheit to Celsius
+// ✌ #10: Calculate the sum of numbers in an array of numbers
+// ✌ #11: Calculate the average of the numbers in an array of numbers
 // #12: Create a function that receives an array of numbers and returns an array containing only the positive numbers
 // #13: Find the maximum number in an array of numbers
 // Skip - #14: Print the first 10 Fibonacci numbers without recursion
