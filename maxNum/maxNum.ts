d962c9b9140d918734f44377c8f21cabe0f280f9
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-22 19:02:05 +0600

    testing

---
+++ maxNum/maxNum.ts
@@ -0,0 +1,9 @@
+// #13: Find the maximum number in an array of numbers
+
+function maxNumber(arr : number[]) {
+    for (let i = 0; i < arr.length; i++){
+
+    }
+}
+
+maxNumber([45, 76, 12, -43, 145.78]);
