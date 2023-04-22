a1847eb7366bb015cc35f7282dcd90af97b19fdc
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-04-22 16:39:04 +0600

    positive numb array

---
+++ positiveArray/positiveArray.ts
@@ -0,0 +1,14 @@
+// #12: Create a function that receives an array of numbers and returns an array containing only the positive numbers
+
+function positiveArray(arr : number[]) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] >= 0) {
+            newArr.push(arr[i])
+        }
+    }
+
+    console.log(newArr);
+}
+
+positiveArray([1, -2, 3, 4, -10, -566, 45.67, 0]);
