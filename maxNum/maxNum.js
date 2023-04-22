35e1042920f264627c52ee81b1283adeff88c3de
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-04-22 21:32:00 +0600

    Solved max num in an array

--- maxNum/maxNum.js
+++ maxNum/maxNum.js
@@ -1,34 +1,22 @@
+"use strict";
 // #13: Find the maximum number in an array of numbers
-
-function maxNumber(arr: number[]) {
-  let maxNumber = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (maxNumber < arr[i]) {
-      maxNumber = arr[i];
+function maxNumber(arr) {
+    let maxNumber = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (maxNumber < arr[i]) {
+            maxNumber = arr[i];
+        }
     }
-  }
-
-  console.log(maxNumber);
+    console.log(maxNumber);
 }
-
 maxNumber([45, 76, 12, -43, 145.78]);
-
 ///////////////////// ------------------ 2nd way ---------------- //////////////////////////
-
 const newArr = [1, 2, 67, -90, 4567];
-
 const maxNum = Math.max(...newArr);
-
 console.log(maxNum);
-
-
 //////////////////// ---------------- 3rd way ---------------------- ////////////////////////
-
-
-function newArray(arr: number[]) {
+function newArray(arr) {
     const maxArray = arr.reduce((a, b) => a > b ? a : b, 0);
     console.log(maxArray);
 }
-
-
-newArray([1, 2, 67, -90, 4567])
+newArray([1, 2, 67, -90, 4567]);
