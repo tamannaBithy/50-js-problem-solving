8a3ed03fa9c4025916cd73859790e5c73d5ce576
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-04-28 18:21:02 +0600

    Solved nth Fibonacci number using recursion

--- firstTenFibonacci/firstTenFibonacci.js
+++ firstTenFibonacci/firstTenFibonacci.js
@@ -10,3 +10,19 @@ for (var i = 2; i < 10; i++) {
     f0 = f1;
     f1 = fi;
 }
+////////////////// ---------------------------- 2nd way ---------------------- /////////////////////////
+let fibonacci = [0, 1];
+for (let i = 2; i < 10; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+console.log(fibonacci);
+// #15: Create a function that will find the nth Fibonacci number using recursion
+function nthFibonacci(n) {
+    if (n <= 1) {
+        return n;
+    }
+    else {
+        return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+    }
+}
+console.log(nthFibonacci(10));
