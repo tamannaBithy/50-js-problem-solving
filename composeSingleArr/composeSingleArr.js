d4f131c2b3b2aa6ec122e812612a2f0f7a296584
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-05-14 20:26:20 +0600

    Solved compose a single arr

---
+++ composeSingleArr/composeSingleArr.js
@@ -0,0 +1,19 @@
+"use strict";
+// #25: Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both
+function composeNewArr(arr1, arr2) {
+    let newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        const numb1 = arr1[i];
+        if (!arr2.includes(numb1)) {
+            newArr.push(numb1);
+        }
+    }
+    for (let j = 0; j < arr2.length; j++) {
+        const numb2 = arr2[j];
+        if (!arr1.includes(numb2)) {
+            newArr.push(numb2);
+        }
+    }
+    console.log(newArr);
+}
+composeNewArr([1, 2, 4, 78, 322], [564, 90, 2, 78, 5]);
