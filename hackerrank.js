c3ffa8f5be2b05590941a88e868eecb21b0554d1
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-10-23 15:29:12 +0600

    template literal in function

--- hackerrank.js
+++ hackerrank.js
@@ -100,6 +100,48 @@ function vowelsAndConsonants(s) {
 
 vowelsAndConsonants("javascriptloops");
 
+function reverseString(s) {
+  const newString = s.split("").reverse().join("");
+  console.log(newString);
+}
+
+reverseString("1234");
+
+function getCount(objects) {
+  let count = 0;
+
+  const newObj = objects.map((obj) => {
+    if (obj.x === obj.y) {
+      count++;
+    }
+  });
+
+  console.log(count);
+}
+
+getCount([
+  { x: 1, y: 2 },
+  { x: 4, y: 4 },
+  { x: 7, y: 7 },
+]);
 
 
 
+
+// day- 5- template literals
+
+function sides(strings, A, P) {
+  const s1 = (P + Math.sqrt(Math.pow(P, 2) - 16 * A)) / 4;
+  const s2 = (P - Math.sqrt(Math.pow(P, 2) - 16 * A)) / 4;
+
+  const newArr = [s1, s2];
+  const result = newArr.sort();
+
+  return result;
+}
+
+const area = 140;
+const perimeter = 48;
+const result = sides`The area is ${area} and the perimeter is ${perimeter}.`;
+
+console.log(result);
